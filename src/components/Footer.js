b1e8ec3a04f38f136eca7985fb7124ae7321a2f7
Shahid Hassan Ansari <ansari.shah28@gmail.com>
2023-03-25 21:54:09 +0530

    Ui fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 function Footer({ onClick, btnText }) {
   return (
-    <div className="w-full bg-slate-100 h-[16dvh] flex px-4 items-center sticky bottom-0 justify-center">
+    <div className="w-full bg-slate-100 h-[10dvh] flex px-4 items-center sticky bottom-0 justify-center">
       <Button onClick={onClick} text={btnText} />
     </div>
   );
--- src/components/QuizHome.js
+++ src/components/QuizHome.js
@@ -15,7 +15,7 @@ const QUIZ_DESCRIPTION =
 function QuizHome({ showBottomBar, setShowBottomBar }) {
   const [showFullDescription, setShowFullDescription] = useState(false);
   return (
-    <div className="min-h-[68vh]">
+    <div className="min-h-[80vh]">
       <img
         src="/assets/quizPoster.jpeg"
         alt="poster"
