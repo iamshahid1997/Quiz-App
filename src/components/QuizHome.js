0e5d6550720a1e1615895b1c525e505d0545581c
Shahid Hassan Ansari <ansari.shah28@gmail.com>
2023-03-25 22:20:56 +0530

    ui fix

--- src/components/QuizHome.js
+++ src/components/QuizHome.js
@@ -15,7 +15,7 @@ const QUIZ_DESCRIPTION =
 function QuizHome({ showBottomBar, setShowBottomBar }) {
   const [showFullDescription, setShowFullDescription] = useState(false);
   return (
-    <div className="min-h-[80dvh]">
+    <div className="min-h-[80vh]">
       <img
         src="/assets/quizPoster.jpeg"
         alt="poster"
